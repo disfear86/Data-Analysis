a20736ec6033de8e1e6c55d692c89f37db49d4a6
Dakis P <bl00m88@hotmail.com>
2017-05-16 23:36:02 +0300

    Create convert_cols.py

---
+++ Udacity_Data_Analysis/convert_cols.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+grades_df = pd.DataFrame(
+    data={'exam1': [43, 81, 78, 75, 89, 70, 91, 65, 98, 87],
+          'exam2': [24, 63, 56, 56, 67, 51, 79, 46, 72, 60]},
+    index=['Andre', 'Barry', 'Chris', 'Dan', 'Emilio',
+           'Fred', 'Greta', 'Humbert', 'Ivan', 'James']
+)
+
+
+def convert_one(grade):
+    if grade >= 90:
+        return 'A'
+    elif grade >= 80:
+        return 'B'
+    elif grade >= 70:
+        return 'C'
+    elif grade >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+
+def convert_grades(grades):
+    '''
+    Returns a new DataFrame of converted
+    numerical to letter grades.
+    '''
+
+    return grades.applymap(convert_one)
+
+new_df = convert_grades(grades_df)
+print new_df
